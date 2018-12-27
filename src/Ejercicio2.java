576e0a5ce6b942e0bc7ec709712c6834f682d860
Gabriel <golobar2012@gmail.com>
2018-12-27 19:07:08 +0100

    Terminado ejercicio 2

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,2 +1,36 @@
+import javax.swing.*;
+import java.util.Arrays;
+
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        String entrada=JOptionPane.showInputDialog("¿Tamaño del primer array?");
+            int cantNumeros=0;
+            if (entrada.equals(null)){
+                System.out.println("Entrada inválida");
+            } else {
+                cantNumeros = Integer.parseInt(entrada);
+            }
+            int[] numero=new int[cantNumeros];
+            for (int i = 0; i < cantNumeros; i++) {
+                numero[i]=Integer.parseInt(JOptionPane.showInputDialog("Introduzca el numero en la posición "+i+" del primer array"));
+            }
+            System.out.println(Arrays.toString(numero));
+
+            int[] invertido=new int[numero.length];
+            int iDato=invertido.length-1;
+            int nDato=0;
+            for (int i:
+                    numero) {
+                invertido[iDato--]=numero[nDato++];
+            }
+            System.out.println(Arrays.toString(invertido));
+            if (numero==invertido){
+                System.out.println("Es capicúa");
+            } else {
+                System.out.println("No es capicúa");
+            }
+        }
+    }
+
+
 }
